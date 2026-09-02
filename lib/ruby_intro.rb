d77438e8b3700ae4055f1f2c0b9388562aeeb198
Ericwangjianqi <ericwangjianqi@163.com>
2026-09-01 18:43:36 -0700

    chips 2.5

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,54 @@
 
 def sum(arr)
   # YOUR CODE HERE
+  total = 0
+  arr.each { |x| total += x }
+  total
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+  arr = arr.sort
+  arr[-1] + arr[-2]
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+  arr.combination(2).any? { |x, y| x + y == n }
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(s)
   # YOUR CODE HERE
+  return false if s.empty? || s.nil?
+  s[0] =~ /[b-df-hj-np-tv-z]/i
 end
 
 def binary_multiple_of_4?(s)
   # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/
+  s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-  # YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  attr_accessor :isbn, :price
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
+
 end
